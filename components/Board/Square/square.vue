a92c9601e9ed10c0b96dd29cbcc4ac85b3e44ff3
aoi <aoi@MacBook-Pro.local>
2022-12-29 11:43:01 +0900

    ボードとマス目表示

---
+++ components/Board/Square/square.vue
@@ -0,0 +1,3 @@
+<template>
+    <div>□</div>
+</template>
---
+++ components/Board/board.vue
@@ -0,0 +1,13 @@
+<template>
+    <Square></Square>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Square from './Square/square.vue';
+
+export default Vue.extend({
+    name: "BoardPage",
+    components: { Square }
+})
+</script>
